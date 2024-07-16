1bbfb439abfecb3b29f4142d8be0acd2a9d1f8b4
Gholamreza Hassanzadeh Tabrizi <gholamreza.hassanzade.1994@gmail.com>
2024-07-16 16:58:01 +0330

    remove code

--- src/configs/app-configurations/mui/material-ui.config.theme.ts
+++ src/configs/app-configurations/mui/material-ui.config.theme.ts
@@ -2,33 +2,15 @@
 import { createTheme, Theme } from '@mui/material/styles';
 
 
-// * Import Constants
-//import { THEME_LIGHT } from '@/constants/themes';
-
-
-const { palette: { augmentColor } } = createTheme();
-
-//const createColor = (mainColor: string) => augmentColor({ color: { main: mainColor } })
-
-
 export const MATERIAL_UI_THEME_LIGHT_CONFIG: Theme = createTheme({
-   /*  direction: 'rtl', */
     typography: {
         fontFamily: 'Roboto',
-       /*  allVariants: {
-            textAlign: "right"
-        }, */
+      
         button: {
             fontSize: '1rem',
         }
 
     },
-    /* palette: {
-        primary: {
-            main: createColor(THEME_LIGHT.PRIMARY).main,
-            contrastText: createColor(THEME_LIGHT.WHITE).main
-        }
-    }, */
     components: {
         MuiButton: {
             styleOverrides: {
@@ -37,28 +19,6 @@ export const MATERIAL_UI_THEME_LIGHT_CONFIG: Theme = createTheme({
                 },
             },
         },
-       /*  MuiTextField: {
-            styleOverrides: {
-                root: {
-                    fontFamily: "Roboto",
-                    width: "100%",
-                    padding : "0",
-                    '& .MuiOutlinedInput-root': {
-                        borderRadius: '6px',
-                        height: '45px', // Adjust height here
-                        '& fieldset': {
-                            borderColor: createColor(THEME_LIGHT.PRIMARY).main, // Default border color
-                        },
-                        '&:hover fieldset': {
-                            borderColor: createColor(THEME_LIGHT.SECONDARY).main, // Border color on hover
-                        },
-                        '&.Mui-focused fieldset': {
-                            borderColor: createColor(THEME_LIGHT.SECONDARY).main, // Border color when focused
-                        },
-                    },
-                }
-            }
-        }, */
         MuiTypography: {
             defaultProps: {
                 variantMapping: {
